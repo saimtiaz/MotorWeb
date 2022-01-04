1d89261e111a4d24f98f86f4206b31d3578fabd7
Saym Imtiaz <saymimtiaz@gmail.com>
2022-01-03 23:53:30 -0600

    Created a basic test

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,9 @@
 #Testing the backend
+import unittest
+from backend import helper
+
+def test_short_array_1():
+    array = [1]
+    key = 0
+    index = helper.binarySearch(array, key)
+    assert index == [1,1]
